df8bc5061935f5b893b7ced96199003c00bdcd88
Mikaela Gurney <mikaelagurney@gmail.com>
2020-07-16 13:16:07 -0700

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Mikaela Gurney`,
+    description: `Mikaela Gurney's homepage`,
+    author: `Mikaela Gurney`,
   },
   plugins: [
     {
