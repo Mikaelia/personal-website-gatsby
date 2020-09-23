6c549549d3fb1d0c84cceb4f58c637c74ea9b1b0
Mikaela Gurney <mikaelagurney@gmail.com>
2020-09-23 12:38:49 -0700

    Update resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -13,7 +13,7 @@ const ResumePage = () => (
     <IframeWrapper>
       <iframe
         title="resume-pdf"
-        src="https://drive.google.com/file/d/1Eit1PE2IE7Q-5u951vCNy-w7Nhhz5wCj/preview"
+        src="https://drive.google.com/file/d/1WtsFfVUbjCcU3b9EMq0fzYBhWeapiqPx/preview"
         width="100%"
         height="100%"
         style={{ marginTop: "2.5rem", height: "calc(100% + 1.5rem)" }}
